b6b2bdf92fd58157c8362f5d14811f7d619f14d2
Rishabh Manocha <rmanocha@gmail.com>
2012-05-20 20:31:41 -0400

    We now have short urls being saved in Redis with unique keys (basic #s for now)

--- src/cl_short/views/welcome.clj
+++ src/cl_short/views/welcome.clj
@@ -17,6 +17,22 @@
                     (forms/long-url-fields long-url)
                     (submit-button "Shorten URL"))))
 
+(defn get-short-url [{:keys [long-url]}]
+  (redis/with-server {:host "127.0.0.1" :port 6379 :db 0}
+    (do
+      (if (nil? (redis/get long-url))
+        (let [next-val
+              (if (nil? (redis/get "next-val"))
+                (redis/set "next-val" 1)
+                (redis/incr "next-val"))
+              full-url (str "http://localhost:8080/" next-val)]
+          (redis/set full-url long-url)
+          (redis/set long-url full-url)
+          full-url))
+        (redis/get long-url))))
+
 (defpage [:post "/short"] {:as long-url}
   (common/layout
-    [:p "Url Shortened"]))
+    (let [short-url (get-short-url long-url)]
+      [:p "Short URL is: "
+         [:a {:href short-url} short-url]])))
