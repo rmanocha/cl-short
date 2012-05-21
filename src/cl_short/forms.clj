a4dd3f5b2d6c03c8605adb8dac947fa69c4496e8
Rishabh Manocha <rmanocha@gmail.com>
2012-05-21 16:08:44 -0400

    Only import the stuff that we need

--- src/cl_short/forms.clj
+++ src/cl_short/forms.clj
@@ -1,6 +1,6 @@
 (ns cl-short.forms
   (:use [noir.core :only [defpartial]]
-        hiccup.form-helpers))
+        [hiccup.form-helpers :only [label text-field]]))
 
 (defpartial long-url-fields [{:keys [long-url]}]
   (label "long-url" "Long URL: ")
