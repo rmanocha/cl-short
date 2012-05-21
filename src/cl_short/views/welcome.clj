e5994520419960160d8594db920d5439804ffd8d
Rishabh Manocha <rmanocha@gmail.com>
2012-05-21 01:28:17 -0400

    This should allow us to work with REDISTOGO environment variables

--- src/cl_short/views/welcome.clj
+++ src/cl_short/views/welcome.clj
@@ -5,12 +5,18 @@
             [cl-short.forms :as forms])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
-        [noir.response :as resp]
+        [noir.response :as rsp]
         hiccup.form-helpers))
 
-(def redis-url (get (System/getenv) "REDIS_URL"))
-(def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
-(def redis-pass (get (System/getenv) "REDIS_PASS"))
+; This came from http://thecomputersarewinning.com/post/clojure-heroku-noir-mongo/
+(defn split-redis-url [url]
+  "Parses redistogo url from heroku, eg. redis://username:password@my.host:6789"
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)$" url)] ;; Setup the regex.
+    (when (.find matcher) ;; Check if it matches.
+      (let [config-map (zipmap [:match :user :password :host :port] (re-groups matcher))]
+        (update-in config-map [:port] #(Integer/parseInt %))))))
+
+(def redis-config (split-redis-url (get (System/getenv) "REDISTOGO_URL")))
 
 (defpage "/short" {:as long-url}
          (common/layout
@@ -19,13 +25,13 @@
                     (submit-button "Shorten URL"))))
 
 (defpage "/:url-key" {:keys [url-key]}
-  (redis/with-server {:host redis-url :port redis-port :password redis-pass}
+  (redis/with-server redis-config
     (do
       (if-let [long-url (redis/get (str "http://localhost:8080/" url-key))]
-        (resp/redirect long-url)))))
+        (rsp/redirect long-url)))))
 
 (defn get-short-url [{:keys [long-url]}]
-  (redis/with-server {:host redis-url :port redis-port :password redis-pass}
+  (redis/with-server redis-config
     (do
       (if-let [surl (redis/get long-url)]
         surl
